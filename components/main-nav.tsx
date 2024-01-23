387ea1565c5a561ee54297aa35c93d11dd6db38e
Harsh <harsh@Harshs-MacBook-Pro.local>
2024-01-23 18:37:38 +0530

    fea: navbar

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import logo from "@/public/assets/fetcch.svg"
 import manta from "@/public/assets/manta.svg"
+import * as Menubar from "@radix-ui/react-menubar"
 
 import ConnectWalletButton from "./connect-wallet-button"
 
@@ -29,12 +32,47 @@ const MainNav = () => {
         >
           Integrate
         </Link>
-        <Link
-          href="#"
-          className="text-[#363C46] transition-all duration-200 hover:text-primary"
-        >
-          Resources
-        </Link>
+        <Menubar.Root>
+          <Menubar.Menu>
+            <Menubar.Trigger className="text-[#363C46] transition-all duration-200 hover:text-primary">
+              Resources
+            </Menubar.Trigger>
+            <Menubar.Portal>
+              <Menubar.Content
+                className="min-w-[220px] rounded-md bg-white p-[5px] will-change-[transform,opacity] [animation-duration:_400ms] [animation-timing-function:_cubic-bezier(0.16,_1,_0.3,_1)] z-50"
+                align="start"
+                sideOffset={5}
+                alignOffset={-3}
+              >
+                <Menubar.Item>
+                  <Link
+                    href="https://writings.fetcch.xyz"
+                    target="_blank"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Writings
+                  </Link>
+                </Menubar.Item>
+                <Menubar.Item>
+                  <Link
+                    href="/report-with-anq"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Reports
+                  </Link>
+                </Menubar.Item>
+                <Menubar.Item>
+                  <Link
+                    href="https://docs.fetcch.xyz/faqs/faqs"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    FAQs
+                  </Link>
+                </Menubar.Item>
+              </Menubar.Content>
+            </Menubar.Portal>
+          </Menubar.Menu>
+        </Menubar.Root>
       </ul>
       <div className="max-w-fit">
         <ConnectWalletButton />
